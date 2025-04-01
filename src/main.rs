f84c0245d85c64e42e5f38d85d97ec0521a28ac4
Thomas Gebert <thomas@gebert.app>
2025-04-01 18:07:30 -0400

    Add my new workspace app in here too.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use clap::Parser;
 use csv::ReaderBuilder;
 use serde_json::{Result, Value};
 use std::result::Result as StdResult;
-use std::{collections::HashMap, error::Error, future::Future, pin::Pin};
+use std::{collections::HashMap,collections::HashSet, error::Error, future::Future, pin::Pin};
 use tokio::io::{self, AsyncBufReadExt, BufReader};
 
 fn parse_pipe_delimited_line(line: &str) -> Vec<String> {
@@ -229,6 +229,51 @@ async fn switch_apps() -> StdResult<(), Box<dyn Error>> {
     Ok(())
 }
 
+
+async fn get_workspace_json() -> StdResult<String, Box<dyn Error>> {
+    let output = Command::new("swaymsg")
+        .arg("-t")
+        .arg("get_workspaces")
+        .output()
+        .await?;
+    return Ok(String::from_utf8_lossy(&output.stdout).as_ref().to_string());
+}
+
+fn workspace_get_value(x : String) -> Result<Value> {
+    let v: Value = serde_json::from_str(x.as_ref())?;
+    return Ok(v); 
+}
+
+async fn create_workspace(work_num : i64) -> StdResult<(), Box<dyn Error>> {
+    Command::new("swaymsg")
+        .arg("workspace")
+        .arg(work_num.to_string())
+        .output() .await?;
+    Ok(())
+}
+
+async fn do_new_workspace() -> StdResult<(),  Box<dyn Error>> {
+    let output = get_workspace_json().await?;
+    let v : Value = workspace_get_value(output)?;
+
+    if let Value::Array(arr) = v {
+        let b: HashSet<i64>= 
+            arr
+            .iter()
+            .filter_map(|workspace| workspace.get("num").and_then(Value::as_i64))
+            .collect();
+        for i in 1..20 {
+            if !b.contains(&i) {
+                create_workspace(i).await;
+                break;
+            }
+        }
+    }
+    Ok(())
+}
+
+
+
 #[tokio::main]
 async fn main() -> StdResult<(), Box<dyn Error>> {
     let args = Args::parse();
@@ -236,15 +281,9 @@ async fn main() -> StdResult<(), Box<dyn Error>> {
     match args.command.as_str() {
         "get-apps" => get_all_apps().await?,
         "switch-apps" => switch_apps().await?,
+        "new-ws" => do_new_workspace().await?,
         _ => (),
     }
     Ok(())
 
-    // if args.command == "get-apps" {
-    //     get_all_apps().await
-    // } else if args.command == "switch-apps" {
-    //     switch_apps().await
-    // } else {
-    //     Ok(())
-    // }
 }
